15fe23f385fc6041f1dd367aa94351377d3c1c52
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-29 12:47:48 +0200

    Boolean Type, Boolean Literals #2

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,20 @@
-const pizzas: number = 2;
+const pizzas: number = 1;
 
 function getDiscount(productNum: number): boolean {
   return productNum >= 3;
 }
 
+function diff(productNum: number): boolean {
+  const diff = 3 - productNum;
+  return diff === 1;
+}
+
 if (getDiscount(pizzas)) {
   console.log(`You get a discount`);
 } else {
-  console.log(`Order ${3 - pizzas} more products to get a discount`);
+  console.log(
+    `Order ${3 - pizzas} more product${
+      diff(pizzas) ? "" : "s"
+    } to get a discount`
+  );
 }
