d8b4ca346827bad52d4fd80da69081f732f91ee2
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-29 11:09:34 +0200

    Array Spread Operator

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,15 @@
-// function sumAll(arrNum: number[]) {
-//   return arrNum.reduce((prev: number, next: number) => {
-//     return prev + next;
-//   });
-// }
+//concat arrays
+const toppings = ["pepperoni", "chilli"];
 
-// console.log(sumAll([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+const newToppings = ["mushrooms", "basil"];
 
-//rest parameter
-// function sumAll(message: string, ...restParameter: number[]) {
-//   console.log(message);
-//   return restParameter.reduce((prev: number, next: number) => {
-//     return prev + next;
-//   });
-// }
+//const allToppings = toppings.concat(newToppings);
 
-// console.log(sumAll("Hello", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+//swap th order
+//const allToppingsSwaped = [newToppings.concat(toppings)];
 
-//with arrow func
-const sumAll = (message: string, ...restParameter: number[]) =>
-  restParameter.reduce((prev: number, next: number) => prev + next);
-
-console.log(sumAll("Hello", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+//use the attay spread operator
+const allToppings = [...toppings, ...newToppings];
+//swap th order
+//const allToppings = [...newToppings, ...toppings ];
+console.log(allToppings);
