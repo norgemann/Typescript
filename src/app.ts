fab350edee6cbe77bcf7811d5af7c21cca8937dd
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-29 21:18:08 +0200

    Diving into Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,112 @@
-//Type Aliases
-type Size = "small" | "medium" | "large";
-type Callback = (size: Size) => void;
-let size: Size = "small";
-
-let pickSize: Callback = (x) => {
-  size = x;
-};
-
-pickSize("medium");
-console.log(size);
-
-//Type Assertions
-
-type Pizza = { name: string; topping: number };
-
-const pizza = { name: "Kaprioza", topping: 5 };
-
-const serialized = JSON.stringify(pizza);
-
-function getNameFromJSON(obj: string): string {
-  return (JSON.parse(obj) as Pizza).name;
+//Creating Interfaces
+interface Pizza {
+  name: string;
+  sizes: string[];
 }
 
-console.log(getNameFromJSON(serialized));
+let ljuta: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes,
+  };
+}
+
+ljuta = createPizza("Ljuta", ["S", "L"]);
+console.log(ljuta);
+
+//Interfaces with Function Types
+interface Car {
+  name: string;
+  sizes: string[];
+  getSizes(): string[];
+}
+
+function createCar(name: string, sizes: string[]): Car {
+  return {
+    name,
+    sizes,
+    getSizes() {
+      return this.sizes;
+    },
+  };
+}
+
+let mali: Car = createCar("Mali auto", ["S", "XS"]);
+console.log(mali.getSizes());
+
+//Extending Interfaces
+
+interface Sizes {
+  sizes: string[];
+}
+
+interface Cookie extends Sizes {
+  name: string;
+  getSizes(): string[];
+}
+
+function createCookie(name: string, sizes: string[]): Cookie {
+  return {
+    name,
+    sizes,
+    getSizes() {
+      return this.sizes;
+    },
+  };
+}
+
+let choco: Cookie = createCookie("Chokomoko", ["S", "XS", "L"]);
+console.log(choco.getSizes());
+
+//Interfaces and Optional Properties
+interface Sizes {
+  sizes: string[];
+}
+
+interface House extends Sizes {
+  name: string;
+  windows?: number;
+  getSizes(): string[];
+}
+
+function createHouse(name: string, sizes: string[]): House {
+  return {
+    name,
+    sizes,
+    getSizes() {
+      return this.sizes;
+    },
+  };
+}
+
+let house: House = createHouse("nice house", ["L"]);
+house.windows = 5;
+console.log(house.getSizes(), house.windows);
+
+//Interfaces with Index Signatures
+
+interface PC extends Sizes {
+  name: string;
+  OS?: number;
+  getSizes(): string[];
+  [key: number]: string;
+  [key: string]: any;
+}
+
+function createPC(name: string, sizes: string[]): PC {
+  return {
+    name,
+    sizes,
+    getSizes() {
+      return this.sizes;
+    },
+  };
+}
+
+let pc: PC = createPC("laptop", ["M"]);
+pc.OS = 7;
+pc[1] = "8";
+pc["a"] = true;
+console.log(pc);
