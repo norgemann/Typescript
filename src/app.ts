4cbefd0225d63971c3d5e05a7822e2297e4c8f75
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-29 21:44:00 +0200

    Understanding Classes and Constructors

--- src/app.ts
+++ src/app.ts
@@ -1,112 +1,33 @@
-//Creating Interfaces
-interface Pizza {
+//before Class
+function Pizza(name: string) {
+  this.name = name;
+  this.topping = "";
+}
+
+Pizza.prototype.addTopping = function addTopping(topping: string) {
+  this.topping = topping;
+};
+
+let pizza = new Pizza("Kapricoza");
+pizza.addTopping("kecap");
+
+console.log(pizza);
+
+//Class
+class SuperPizza {
   name: string;
-  sizes: string[];
+  topping: string;
+  constructor(name: string) {
+    this.name = name;
+    this.topping = "";
+  }
+
+  addTopping(topping: string) {
+    this.topping = topping;
+  }
 }
 
-let ljuta: Pizza;
+let superPizza = new SuperPizza("Kapricoza");
+superPizza.addTopping("kecap");
 
-function createPizza(name: string, sizes: string[]): Pizza {
-  return {
-    name,
-    sizes,
-  };
-}
-
-ljuta = createPizza("Ljuta", ["S", "L"]);
-console.log(ljuta);
-
-//Interfaces with Function Types
-interface Car {
-  name: string;
-  sizes: string[];
-  getSizes(): string[];
-}
-
-function createCar(name: string, sizes: string[]): Car {
-  return {
-    name,
-    sizes,
-    getSizes() {
-      return this.sizes;
-    },
-  };
-}
-
-let mali: Car = createCar("Mali auto", ["S", "XS"]);
-console.log(mali.getSizes());
-
-//Extending Interfaces
-
-interface Sizes {
-  sizes: string[];
-}
-
-interface Cookie extends Sizes {
-  name: string;
-  getSizes(): string[];
-}
-
-function createCookie(name: string, sizes: string[]): Cookie {
-  return {
-    name,
-    sizes,
-    getSizes() {
-      return this.sizes;
-    },
-  };
-}
-
-let choco: Cookie = createCookie("Chokomoko", ["S", "XS", "L"]);
-console.log(choco.getSizes());
-
-//Interfaces and Optional Properties
-interface Sizes {
-  sizes: string[];
-}
-
-interface House extends Sizes {
-  name: string;
-  windows?: number;
-  getSizes(): string[];
-}
-
-function createHouse(name: string, sizes: string[]): House {
-  return {
-    name,
-    sizes,
-    getSizes() {
-      return this.sizes;
-    },
-  };
-}
-
-let house: House = createHouse("nice house", ["L"]);
-house.windows = 5;
-console.log(house.getSizes(), house.windows);
-
-//Interfaces with Index Signatures
-
-interface PC extends Sizes {
-  name: string;
-  OS?: number;
-  getSizes(): string[];
-  [key: number]: string;
-  [key: string]: any;
-}
-
-function createPC(name: string, sizes: string[]): PC {
-  return {
-    name,
-    sizes,
-    getSizes() {
-      return this.sizes;
-    },
-  };
-}
-
-let pc: PC = createPC("laptop", ["M"]);
-pc.OS = 7;
-pc[1] = "8";
-pc["a"] = true;
-console.log(pc);
+console.log(superPizza);
