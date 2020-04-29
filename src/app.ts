824b19a72f9a3d648c54495995d6e1e346d250e8
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-29 09:55:44 +0200

    Arrow functions and implicit returns

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,28 @@
-console.log('Ahoy TypeScript!');
+const pizzas = [{ name: "Pepperoni", toppings: ["pepperoni"] }];
+
+//not an arrow function
+// const mappedPizzas = pizzas.map(function (pizza) {
+//   return pizza.name.toUpperCase();
+// });
+
+//arrow function
+const mappedPizzas = pizzas.map((pizza) => pizza.name.toUpperCase());
+
+console.log(mappedPizzas);
+
+const pizza = {
+  name: "Blazing Inferno",
+  //   getName: function () {
+  //     //console.log(this);
+  //     //const that = this;
+  //     // setTimeout(() => {
+  //     //   console.log(this.name);
+  //     // }, 100);
+  //   },
+  //getName: () => pizza.name,
+  getName: function () {
+    return this.name;
+  },
+};
+
+console.log(pizza.getName());
