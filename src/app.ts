481b04051150d66afe985701dc8802506a6213a2
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 07:53:39 +0200

    Static Properties and Methods

--- src/app.ts
+++ src/app.ts
@@ -1,44 +1,15 @@
-interface SizesInterface {
-  //we can not add sizes property since it is protected(same for private members)
-  availableSizes: string[];
-}
+const date = new Date();
+console.log(date.getUTCHours());
 
-abstract class Sizes implements SizesInterface {
-  constructor(protected sizes: string[]) {}
+//static, no new instance
+const dateNowStatic = Date.now();
+console.log(dateNowStatic);
 
-  get availableSizes() {
-    return this.sizes;
-  }
-
-  set availableSizes(sizes: string[]) {
-    this.sizes = sizes;
+class Coupon {
+  static couponName: string = "Super Coupon";
+  static createCoupon(percentage: number) {
+    return `PIZZA_RESTAURANTS_${this.couponName}_${percentage}%`;
   }
 }
 
-interface PizzaInterface extends SizesInterface {
-  readonly name: string;
-  addSize(size: string[]): void;
-  addTopping(topping: string): void;
-}
-
-class Pizza extends Sizes implements PizzaInterface {
-  constructor(
-    readonly name: string,
-    private topping: string[],
-    sizes: string[]
-  ) {
-    super(sizes);
-  }
-
-  addSize(size: string[]) {
-    this.sizes = [...this.sizes, ...size];
-  }
-
-  addTopping(topping: string) {
-    this.topping.push(topping);
-  }
-}
-
-let pizza = new Pizza("Kapricoza", ["pepperoni", "mushrooms"], ["L", "XL"]);
-pizza.addSize(["S", "M"]);
-console.log(pizza);
+console.log(Coupon.createCoupon(5))
