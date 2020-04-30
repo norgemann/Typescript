f938b6e354e0d2c0f9653ed97a3d628c21ab6821
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 07:04:56 +0200

    Setters and Getters (Accessors)

--- src/app.ts
+++ src/app.ts
@@ -1,69 +1,31 @@
-//before Class
-// function Pizza(name: string) {
-//   this.name = name;
-//   this.topping = "";
+//Setters and Getters (Accessors)
+
+class Sizes {
+  constructor(private sizes: string[]) {}
+
+  get availableSizes() {
+    return this.sizes;
+  }
+
+  set availableSizes(sizes: string[]) {
+    this.sizes = sizes;
+  }
+}
+
+let pizzaSizes = new Sizes(["S", "M", "L"]);
+
+console.log(pizzaSizes.availableSizes);
+pizzaSizes.availableSizes = ["S", "M", "L", "XL"];
+console.log(pizzaSizes.availableSizes);
+
+// class Pizza {
+//   constructor(readonly name: string, private topping: string[]) {}
+
+//   addTopping(topping: string) {
+//     this.topping.push(topping);
+//   }
 // }
 
-// Pizza.prototype.addTopping = function addTopping(topping: string) {
-//   this.topping = topping;
-// };
-
-// let pizza = new Pizza("Kapricoza");
+// let pizza = new Pizza("Kapricoza", ["pepperoni", "mushrooms"]);
 // pizza.addTopping("kecap");
-
 // console.log(pizza);
-
-//Class
-class SuperPizza {
-  name: string;
-  topping: string;
-  constructor(name: string) {
-    this.name = name;
-    this.topping = "";
-  }
-
-  addTopping(topping: string) {
-    this.topping = topping;
-  }
-}
-
-let superPizza = new SuperPizza("Kapricoza");
-superPizza.addTopping("kecap");
-
-console.log(superPizza);
-
-//Public and Private Members
-class PublicSuperPizza {
-  constructor(private name: string, private topping: string[]) {}
-
-  addTopping(topping: string) {
-    this.topping.push(topping);
-  }
-}
-
-let publicSuperPizza = new PublicSuperPizza("Kapricoza", [
-  "pepperoni",
-  "mushrooms",
-]);
-publicSuperPizza.addTopping("kecap");
-
-console.log(publicSuperPizza);
-
-//Readonly Members
-class ReadonlyPizza {
-  constructor(readonly name: string, private topping: string[]) {}
-
-  addTopping(topping: string) {
-    this.topping.push(topping);
-  }
-}
-
-let readonlyPizza = new ReadonlyPizza("Kapricoza", [
-  "pepperoni",
-  "mushrooms",
-]);
-readonlyPizza.addTopping("kecap");
-
-//readonlyPizza.name = "Capricioza" // error Cannot assign to 'name' because it is a read-only property.!!!
-
-console.log(readonlyPizza);
