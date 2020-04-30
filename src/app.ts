cebc88991a3ef7a17d7eadaf12136b9dc21f1cea
Marko Ivanovic <marko.ivanovic@Soprex.local>
2020-04-30 06:47:15 +0200

    Classes, Properties and Inheritance

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,17 @@
 //before Class
-function Pizza(name: string) {
-  this.name = name;
-  this.topping = "";
-}
+// function Pizza(name: string) {
+//   this.name = name;
+//   this.topping = "";
+// }
 
-Pizza.prototype.addTopping = function addTopping(topping: string) {
-  this.topping = topping;
-};
+// Pizza.prototype.addTopping = function addTopping(topping: string) {
+//   this.topping = topping;
+// };
 
-let pizza = new Pizza("Kapricoza");
-pizza.addTopping("kecap");
+// let pizza = new Pizza("Kapricoza");
+// pizza.addTopping("kecap");
 
-console.log(pizza);
+// console.log(pizza);
 
 //Class
 class SuperPizza {
@@ -31,3 +31,39 @@ let superPizza = new SuperPizza("Kapricoza");
 superPizza.addTopping("kecap");
 
 console.log(superPizza);
+
+//Public and Private Members
+class PublicSuperPizza {
+  constructor(private name: string, private topping: string[]) {}
+
+  addTopping(topping: string) {
+    this.topping.push(topping);
+  }
+}
+
+let publicSuperPizza = new PublicSuperPizza("Kapricoza", [
+  "pepperoni",
+  "mushrooms",
+]);
+publicSuperPizza.addTopping("kecap");
+
+console.log(publicSuperPizza);
+
+//Readonly Members
+class ReadonlyPizza {
+  constructor(readonly name: string, private topping: string[]) {}
+
+  addTopping(topping: string) {
+    this.topping.push(topping);
+  }
+}
+
+let readonlyPizza = new ReadonlyPizza("Kapricoza", [
+  "pepperoni",
+  "mushrooms",
+]);
+readonlyPizza.addTopping("kecap");
+
+//readonlyPizza.name = "Capricioza" // error Cannot assign to 'name' because it is a read-only property.!!!
+
+console.log(readonlyPizza);
